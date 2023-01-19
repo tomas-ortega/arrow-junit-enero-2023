4f9b93d3015598af767df0f403de03fccba7aeac
tomy <m.tomas.ortega@gmail.com>
2023-01-19 12:23:07 +0100

    Add first iteration rest api module

---
+++ patient-his-rest-api/src/main/java/com/arrow/his/App.java
@@ -0,0 +1,13 @@
+package com.arrow.his;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        PatientDTO patient;
+    }
+}
---
+++ patient-his-rest-api/src/test/java/com/arrow/his/AppTest.java
@@ -0,0 +1,29 @@
+package com.arrow.his;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
