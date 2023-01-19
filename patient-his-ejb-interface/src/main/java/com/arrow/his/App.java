ccabb495b7ed438669912a208b36fb9271121d2d
tomy <m.tomas.ortega@gmail.com>
2023-01-19 13:26:01 +0100

    Add ejb interface module

--- patient-his-ejb-interface/src/main/java/com/arrow/his/App.java
+++ patient-his-ejb-interface/src/main/java/com/arrow/his/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        PatientDTO patient;
+        System.out.println( "Hello World!" );
     }
 }
---
+++ patient-his-rest-api/src/main/java/com/arrow/his/PatientRestAPI.java
@@ -0,0 +1,27 @@
+package com.arrow.his;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class PatientRestAPI {
+    public Response searchPatient() {
+        Response httpResponse = null;
+        
+        try {
+
+            return httpResponse;
+        } catch(Exception ex) {
+
+            return httpResponse;
+
+        }
+
+        
+    }
+}
+
+/*IP:PORT/<CONTEXT>/api/microservice1
+IP:PORT/<CONTEXT>/api/microservice2
+
+http://127.0.0.1:8080/his/api/*/
